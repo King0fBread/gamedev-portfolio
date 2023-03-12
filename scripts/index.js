ac6f5d641277db4b3543bdf0148f5fbff7943af4
Nikita Glubokov <ITVenomancer@gmail.com>
2023-03-12 12:27:22 +0300

    Improved menu functionality; working on basic layout

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,39 @@
 window.addEventListener('touchmove', (e)=> e.preventDefault());
 let containerCollection = document.getElementsByClassName("container");
-console.log(containerCollection)
-
-let navbar = document
-      .getElementById("menu-buttons")
-      .querySelectorAll('li');
-navbar[1].onclick=()=>{
-  swapContainerContent();
+let navbar = document.getElementById("menu-buttons").querySelectorAll('li');
+highlightCurrentActiveMenu(0);
+ 
+//listening for mouse events
+for(let i = 0; i<navbar.length; i++){
+  navbar[i].onclick = () => {
+    swapContainerContent(i)
+    highlightCurrentActiveMenu(i)
+  }
+  // navbar[i].onmouseover = () =>{
+  //   highlightMenuOnMouseOver(i)
+  // }
 }
-
+function highlightMenuOnMouseOver(index){
+  for(let i = 0; i<navbar.length; i++){
+    if(i == chosenIndex){
+      navbar[i].style.color = "grey"
+    }
+    else{
+      navbar[i].style.color = "white"
+    }
+  }
+}
+function highlightCurrentActiveMenu(index){
+  for(let i = 0; i<navbar.length; i++){
+    if(i == chosenIndex){
+      navbar[i].style.fontWeight = "600"
+    }
+    else{
+      navbar[i].style.fontWeight = "initial"
+    }
+  }
+}
+//swapping the contents of the the container div
 function swapContainerContent(i){
   for(let index = 0; index<containerCollection.length; index++){
     if(index!=i){
@@ -18,7 +43,4 @@ function swapContainerContent(i){
       containerCollection[index].style.display = "block";
     }
   }
-}
-for(let i = 0; i<navbar.length; i++){
-  navbar[i].onclick = () => swapContainerContent(i)
 }
