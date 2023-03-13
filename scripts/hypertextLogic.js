162334dcd489f5adf60ce5d8d27536b9ec905de8
Nikita Glubokov <ITVenomancer@gmail.com>
2023-03-13 09:31:17 +0300

    Working hypertext logic

--- scripts/hypertextLogic.js
+++ scripts/hypertextLogic.js
@@ -1,7 +1,6 @@
 window.onload = manageHypertext()
 
 function manageHypertext(){
-  let hypertextElements = document.getElementsByClassName("hypertextElement");
   let projectsLink = document.getElementById("projectsLink")
   let contactsLink = document.getElementById("contactsLink")
   projectsLink.onclick = () => {
@@ -11,13 +10,5 @@ function manageHypertext(){
   contactsLink.onclick = () => {
     swapContainerContent(3)
     highlightCurrentActiveMenu(3)
-  }
-}
-for(let i of hypertextElements){
-  i.onmouseenter = () => {
-    i.style.cursor = "pointer"
-  }
-  i.onmouseenter =() => {
-    i.style.cursor = "default"
   }
 }
