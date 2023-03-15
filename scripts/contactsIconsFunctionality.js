2f185137d02f9db8fad79e7297ac61d1bcd9e234
Nikita Glubokov <ITVenomancer@gmail.com>
2023-03-15 08:16:42 +0300

    Added functionality to the contacts icons

---
+++ scripts/contactsIconsFunctionality.js
@@ -0,0 +1,12 @@
+function openMail(){
+  window.open("mailto:breadletters@gmail.com")
+}
+function openLinkedIn(){
+  window.open("https://www.linkedin.com/in/nikita-glubokov-947560262/")
+}
+function openGitHub(){
+  window.open("https://github.com/King0fBread")
+}
+function openItchIo(){
+  window.open("https://kingofbread.itch.io/")
+}
