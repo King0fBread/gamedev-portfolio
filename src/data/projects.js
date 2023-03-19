580ff86708cb770d72812a365f93f8a19022f4e3
Nikita Glubokov <ITVenomancer@gmail.com>
2023-03-19 18:06:31 +0300

    Separating the text lines

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ export default [
         name: 'Mizkif: Emote Madness',
         imagePath: '/gameCoversMizkif.png',
         description: "This 2D sidescroller was my first officially released project, where I was learning everything on-the-go, which was certainly exciting and challenging.",
-        highlights: "Here are some of the interesting highlights of my development process: -Utilized Tilemaps for creating several levels with various types of enemies -Created a simple dialogue system -Implemented a state machine for the Main Boss -Added sound control, respawning and scene management",
+        highlights: "Here are some of the interesting highlights of my development process: \n-Utilized Tilemaps for creating several levels with various types of enemies \n-Created a simple dialogue system \n-Implemented a state machine for the Main Boss \n-Added sound control, respawning and scene management",
         video: '/video-previews/mizkifPreview.mp4'
         
     },
@@ -13,7 +13,7 @@ export default [
         name: 'Ancient Evil: The Guessing Game',
         imagePath: '/gameCoversAncientEvil.png',
         description: 'Through the making of this mobile horror puzzle game I became more familiar with the 3D development workflow, which served as a great starting point for all my future releases.',
-        highlights: 'Here are some of the interesting highlights of my development process: -Implemented a “Wordle-like“ mechanic, where the player has to guess a random word in several attempts -Added a death timer for the guessing process, that activates “anomalies” in the world, which player has to deal with to survive. -Utilized “Unity Remote” testing environment on my phone, to personally playtest each development stage -Released the game for Android devices',
+        highlights: 'Here are some of the interesting highlights of my development process: \n-Implemented a “Wordle-like“ mechanic, where the player has to guess a random word in several attempts \n-Added a death timer for the guessing process, that activates “anomalies” in the world, which player has to deal with to survive. \n-Utilized “Unity Remote” testing environment on my phone, to personally playtest each development stage \n-Released the game for Android devices',
         video: '/video-previews/ancientPreview.mp4'
     },
     {
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import projects from '../data/projects';
 const GamePage = () => {
   const {id} = useParams();
-  const currentProj = projects[id]
+  const [currentProj, setCurrentProj] = useState({})
+  const [highlights, setHighlights] = useState([])
+  useEffect( () => {
+    setCurrentProj(projects[id])
+    setHighlights(currentProj.highlights)
+  }, [])
   return(<div>
     <h1 className='project-page-title'>{currentProj.name}</h1>
     <video src={currentProj.video} width="500" height="300" controls></video>
+    {highlights.map((el, index) => {return(
+      <p key={index}>{el}</p>
+    )})}
   </div>)
-
-}
-function addLineBreaks(text){
-  return text.replace("\n", "<br />")
 }
 export default GamePage;
